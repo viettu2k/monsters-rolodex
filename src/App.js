782bec17eea49deb4efdb4f12bcfb44861b7df51
viettu2k <doanviettu2k@gmail.com>
2021-06-17 21:31:17 +0700

    update hook

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
+function App() {
+    const [monsters, setMonsters] = useState([]);
+    const [searchField, setSearchField] = useState("");
 
-    this.state = {
-      monsters: [],
-      searchField: "",
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((users) => setMonsters(users));
+    }, []);
+
+    const handleChange = (event) => {
+        setSearchField(event.target.value);
     };
-  }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
-  }
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
-  };
-  render() {
-    const { monsters, searchField } = this.state;
+
     const fillteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
-      <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearchBox
-          placeholder="search monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={fillteredMonsters} />
-      </div>
+        <div className="App">
+            <h1>Monster Rolodex</h1>
+            <SearchBox
+                placeholder="search monsters"
+                handleChange={handleChange}
+            />
+            <CardList monsters={fillteredMonsters} />
+        </div>
     );
-  }
 }
 
 export default App;
